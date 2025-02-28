732e0bd2906c8e780014e94104f8c788d7d72989
Alireza <rostmi83@gmail.com>
2025-02-28 17:53:58 +0330

    fix font for all files

--- src/packages/thankYouPage.java
+++ src/packages/thankYouPage.java
@@ -13,6 +13,7 @@ public class thankYouPage {
 
         // Create thank you label
         JLabel thankYouLabel = new JLabel("با تشکر از مشارکت شما!");
+        thankYouLabel.setFont(font);
         thankYouLabel.setBounds(10, 10, 200, 20);
 
         // Add components to the panel
